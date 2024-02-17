4da4bcd48b27b6cdbe1b460d207329d09f97da20
Mohammed Hatem Monir <atmelshiekh@yahoo.com>
2024-02-17 18:57:05 +0200

    Update GlobalNavBar.jsx

--- src/pages/GlobalNavBar.jsx
+++ src/pages/GlobalNavBar.jsx
@@ -10,44 +10,42 @@ export default function GlobalNavBar() {
 return(<>
 
 <>
-
-<Navbar expand="lg" className="fixed-top bg-body-tertiary shadow my-nav">
+    <Navbar expand="lg" className="fixed-top bg-body-tertiary shadow my-nav">
         <Container>
-          <Navbar.Brand>
-            <Link to="/" className="navbar-brand text-success fw-semibold">
-              Dine Me
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto justify-content-end w-100 ">
-                <ThemeController />
-              <Link to="/" className="nav-link active">
-                Home
-              </Link>
-              <Link to="/menu" className="nav-link">
-                Menu
-              </Link>
-              <Link to="/about" className="nav-link">
-                About
-              </Link>
-              <Link to="/contact" className="nav-link">
-                Contact
-              </Link>
-              <Link to="/Signup" className="nav-link">
-                Sign up
-              </Link>
-              <Link to="/Login" className="nav-link">
-                Login
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
+            <Navbar.Brand>
+                <Link to="/" className="navbar-brand text-success fw-semibold">
+                Dine Me
+                </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-          <div className="xyz">DarkModeTest</div>
+            <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto justify-content-end w-100 ">
+                        <ThemeController />
+                        <Link to="/" className="nav-link active">
+                            Home
+                        </Link>
+                        <Link to="/menu" className="nav-link">
+                            Menu
+                        </Link>
+                        <Link to="/about" className="nav-link">
+                            About
+                        </Link>
+                        <Link to="/contact" className="nav-link">
+                            Contact
+                        </Link>
+                        <Link to="/Signup" className="nav-link">
+                            Sign up
+                        </Link>
+                        <Link to="/Login" className="nav-link">
+                            Login
+                        </Link>
+                    </Nav>
+            </Navbar.Collapse>
+            <div className="xyz">DarkModeTest</div>
         </Container>
-      </Navbar>
-
-      </>
+    </Navbar>
+</>
 
 </>)
 
