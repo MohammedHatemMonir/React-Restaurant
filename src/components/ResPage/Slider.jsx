4091aef01bf521ed08c5d6f06467f4f4862276fc
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-02-22 20:05:16 +0200

    Nav and Slider updated

--- src/components/ResPage/Slider.jsx
+++ src/components/ResPage/Slider.jsx
@@ -1,17 +1,52 @@
 import React, { useState } from "react";
 import "./Slider.scss";
 
+import img1 from "./images/img1.png";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
+import img5 from "./images/img5.jpg";
+
 const NavWithSlider = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [direction, setDirection] = useState("next");
 
+  // Define your slider items with different images and descriptions
+  const sliderItems = [
+    {
+      src: img1,
+      description: "Description for item 1",
+    },
+    {
+      src: img2,
+      description: "Description for item 2",
+    },
+    {
+      src: img3,
+      description: "Description for item 3",
+    },
+    {
+      src: img4,
+      description: "Description for item 4",
+    },
+    {
+      src: img5,
+      description: "Description for item 5",
+    },
+    // Add more slider items as needed
+  ];
+
   const handlePrev = () => {
-    setActiveIndex((prevIndex) => (prevIndex === 0 ? 4 : prevIndex - 1));
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? sliderItems.length - 1 : prevIndex - 1
+    );
     setDirection("prev");
   };
 
   const handleNext = () => {
-    setActiveIndex((prevIndex) => (prevIndex === 4 ? 0 : prevIndex + 1));
+    setActiveIndex((prevIndex) =>
+      prevIndex === sliderItems.length - 1 ? 0 : prevIndex + 1
+    );
     setDirection("next");
   };
 
@@ -21,26 +56,17 @@ const NavWithSlider = () => {
 
   return (
     <div className="my-slider">
-     
       <div className={`slider ${direction}`}>
         <div className="list">
-          {Array.from({ length: 5 }, (_, index) => (
+          {sliderItems.map((item, index) => (
             <div
               key={index}
               className={`item ${index === activeIndex ? "active" : ""}`}
             >
-              <img
-                src="http://image.shutterstock.com/z/stock-vector-food-restaurant-logo-icon-264413183.jpg"
-                alt={`Slider ${index + 1}`}
-              />
+              <img src={item.src} alt={`Slider ${index + 1}`} />
               <div className="content">
-                <p>design</p>
                 <h2>Slider {index + 1}</h2>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Labore, neque? Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Ipsum, ex.
-                </p>
+                <p>{item.description}</p>
               </div>
             </div>
           ))}
@@ -54,17 +80,14 @@ const NavWithSlider = () => {
           </button>
         </div>
         <div className="thumbnail">
-          {Array.from({ length: 5 }, (_, index) => (
+          {sliderItems.map((item, index) => (
             <div
               key={index}
               className={`item ${index === activeIndex ? "active" : ""}`}
               onClick={() => handleThumbnailClick(index)}
             >
-              <img
-                src="http://image.shutterstock.com/z/stock-vector-food-restaurant-logo-icon-264413183.jpg"
-                alt={`Thumbnail ${index + 1}`}
-              />
-              <div className="content">Name Slider</div>
+              <img src={item.src} alt={`Thumbnail ${index + 1}`} />
+              <div className="content">{`Slider ${index + 1}`}</div>
             </div>
           ))}
         </div>
