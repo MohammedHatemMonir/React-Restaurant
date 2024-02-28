eb5e07e877e2c379d4c6342059be6b605b58af64
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-02-28 07:38:16 +0200

    Tailwind installed but not used

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,11 +4,11 @@ import "./Card.scss";
 import Stars1 from "../Stars/Stars";
 import HeartIcon from "../../Icons/HeartIcon";
 import HeartFullIcon from "../../Icons/HeartFullIcon";
-const Card = ({ title, img, id, stars, mealImg, reviewsNum,resName, fav }) => {
+const Card = ({ title, img, id, stars, mealImg, reviewsNum, resName, fav }) => {
   // To Generate Random ID
   const randomId = uuidv4();
   return (
-    <div className="my-card-styels ">
+    <div className="my-card-styels">
       <section className="main-content">
         <div className="container edit-container">
           <div className="row">
@@ -17,8 +17,11 @@ const Card = ({ title, img, id, stars, mealImg, reviewsNum,resName, fav }) => {
                 <div className="food-card_img position-relative">
                   <img src={mealImg} alt={`${title}-Image`} />
                   <a href="/" className="card-heart ">
-                    {!fav? <HeartIcon className="MenuHeart" /> : <HeartFullIcon className="MenuHeart" color="red" />}
-
+                    {!fav ? (
+                      <HeartIcon className="MenuHeart" />
+                    ) : (
+                      <HeartFullIcon className="MenuHeart" color="red" />
+                    )}
                   </a>
                 </div>
                 <div className="food-card_content">
@@ -34,7 +37,7 @@ const Card = ({ title, img, id, stars, mealImg, reviewsNum,resName, fav }) => {
                       <div className="myZoom w-25 h-25">
                         <Stars1 id={randomId + id} stars1={stars} />
                       </div>
-                      <p className="myFont">{`${reviewsNum}`}  </p>
+                      <p className="myFont">{`${reviewsNum}`} </p>
                       <p className="text-dark my-res-title">{resName}</p>
                     </div>
                   </div>
