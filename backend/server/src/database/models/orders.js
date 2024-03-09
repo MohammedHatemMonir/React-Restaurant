670e037078fb83669eb270adf9909c5f6fb5e303
Mustafa Shehata <mustafashehata55@gmail.com>
2024-03-09 20:39:44 +0200

    Create Orders DB Initial Schema

---
+++ backend/server/src/database/models/orders.js
@@ -0,0 +1,32 @@
+
+const mongoose = require("mongoose");
+
+const ordersSchema=mongoose.Schema({
+    ResName:{
+        type:String,
+        require:[true,"Enter resturant name "]
+    },
+    ResImg:{
+        type:String,
+        require:[true,"Enter resturant ResImg"]
+    },
+    Categoery:{
+        type:String,
+        require:[true,"Enter resturant Categoery"]
+    },
+    rating:{
+        type:Number,
+        require:[true,"Enter resturant rating"]
+    },
+    // Mshehata addition to schema
+    creation_date: {
+        type:Date,
+        require:[true,"creation date of restaurant"]
+    },
+    comment_num:{
+        type:Number,
+        require:[true,"Enter resturant rating"]
+    },
+})
+
+module.exports = mongoose.model("order", ordersSchema);
