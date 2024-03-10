14078422bae857e4f487797a03870240104d8e94
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-03-10 22:25:45 +0200

    Cards added in main page

---
+++ src/components/ResPage/New folder/EltestBox.jsx
@@ -0,0 +1,434 @@
+import { TbTruckDelivery } from "react-icons/tb";
+import { IoMdCard } from "react-icons/io";
+
+const EltestBox = () => {
+  return (
+    <div className="restaurant-card restaurant-delivery-card col-md-5 col-sm-8 col-xs-16">
+      <div className="card-inner">
+        <img
+          src="https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Normal/92edf264-8a29-4efb-997a-ca2fdf9b3a92.jpg"
+          alt=""
+          style={{ width: "100%" }}
+        />
+        <div className="card-content clickable-item">
+          <a href="/cairo/chilis-79yv" className="clickable-anchor" />
+          <div>
+            <div className="pull-right flip">
+              <div
+                data-v-34cbeed1=""
+                className="vue-star-rating vue-star-rating-inline"
+              >
+                <div data-v-34cbeed1="" className="vue-star-rating">
+                  {/* Star 1 */}
+                  <span
+                    data-v-34cbeed1=""
+                    className="vue-star-rating-star"
+                    style={{ marginRight: 0 }}
+                  >
+                    <svg
+                      data-v-21f5376e=""
+                      data-v-34cbeed1=""
+                      height={14}
+                      width={14}
+                      viewBox="0 0 14 14"
+                      className="vue-star-rating-star"
+                      step={10}
+                    >
+                      <linearGradient
+                        data-v-21f5376e=""
+                        id="obch1e"
+                        x1={0}
+                        x2="100%"
+                        y1={0}
+                        y2={0}
+                      >
+                        <stop
+                          data-v-21f5376e=""
+                          offset="100%"
+                          stopColor="#faad1d"
+                        />
+                        <stop
+                          data-v-21f5376e=""
+                          offset="100%"
+                          stopColor="#d8d8d8"
+                        />
+                      </linearGradient>
+                      <filter
+                        data-v-21f5376e=""
+                        id="dz1rih"
+                        height="130%"
+                        width="130%"
+                        filterUnits="userSpaceOnUse"
+                      >
+                        <feGaussianBlur
+                          data-v-21f5376e=""
+                          stdDeviation={0}
+                          result="coloredBlur"
+                        />
+                        <feMerge data-v-21f5376e="">
+                          <feMergeNode data-v-21f5376e="" in="coloredBlur" />
+                          <feMergeNode data-v-21f5376e="" in="SourceGraphic" />
+                        </feMerge>
+                      </filter>
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#obch1e)"
+                        stroke="#fff"
+                        filter="url(#dz1rih)"
+                      />
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#obch1e)"
+                        stroke="#999"
+                        strokeWidth={0}
+                        strokeLinejoin="miter"
+                      />
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#obch1e)"
+                      />
+                    </svg>
+                  </span>
+                  {/* Star 2 */}
+                  <span
+                    data-v-34cbeed1=""
+                    className="vue-star-rating-star"
+                    style={{ marginRight: 0 }}
+                  >
+                    <svg
+                      data-v-21f5376e=""
+                      data-v-34cbeed1=""
+                      height={14}
+                      width={14}
+                      viewBox="0 0 14 14"
+                      className="vue-star-rating-star"
+                      step={10}
+                    >
+                      <linearGradient
+                        data-v-21f5376e=""
+                        id="ceakv6j"
+                        x1={0}
+                        x2="100%"
+                        y1={0}
+                        y2={0}
+                      >
+                        <stop
+                          data-v-21f5376e=""
+                          offset="100%"
+                          stopColor="#faad1d"
+                        />
+                        <stop
+                          data-v-21f5376e=""
+                          offset="100%"
+                          stopColor="#d8d8d8"
+                        />
+                      </linearGradient>
+                      <filter
+                        data-v-21f5376e=""
+                        id="zicnqo"
+                        height="130%"
+                        width="130%"
+                        filterUnits="userSpaceOnUse"
+                      >
+                        <feGaussianBlur
+                          data-v-21f5376e=""
+                          stdDeviation={0}
+                          result="coloredBlur"
+                        />
+                        <feMerge data-v-21f5376e="">
+                          <feMergeNode data-v-21f5376e="" in="coloredBlur" />
+                          <feMergeNode data-v-21f5376e="" in="SourceGraphic" />
+                        </feMerge>
+                      </filter>
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#ceakv6j)"
+                        stroke="#fff"
+                        filter="url(#zicnqo)"
+                      />
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#ceakv6j)"
+                        stroke="#999"
+                        strokeWidth={0}
+                        strokeLinejoin="miter"
+                      />
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#ceakv6j)"
+                      />
+                    </svg>
+                  </span>
+                  {/* Star 3 */}
+                  <span
+                    data-v-34cbeed1=""
+                    className="vue-star-rating-star"
+                    style={{ marginRight: 0 }}
+                  >
+                    <svg
+                      data-v-21f5376e=""
+                      data-v-34cbeed1=""
+                      height={14}
+                      width={14}
+                      viewBox="0 0 14 14"
+                      className="vue-star-rating-star"
+                      step={10}
+                    >
+                      <linearGradient
+                        data-v-21f5376e=""
+                        id="tyk07n"
+                        x1={0}
+                        x2="100%"
+                        y1={0}
+                        y2={0}
+                      >
+                        <stop
+                          data-v-21f5376e=""
+                          offset="100%"
+                          stopColor="#faad1d"
+                        />
+                        <stop
+                          data-v-21f5376e=""
+                          offset="100%"
+                          stopColor="#d8d8d8"
+                        />
+                      </linearGradient>
+                      <filter
+                        data-v-21f5376e=""
+                        id="s2i7rr"
+                        height="130%"
+                        width="130%"
+                        filterUnits="userSpaceOnUse"
+                      >
+                        <feGaussianBlur
+                          data-v-21f5376e=""
+                          stdDeviation={0}
+                          result="coloredBlur"
+                        />
+                        <feMerge data-v-21f5376e="">
+                          <feMergeNode data-v-21f5376e="" in="coloredBlur" />
+                          <feMergeNode data-v-21f5376e="" in="SourceGraphic" />
+                        </feMerge>
+                      </filter>
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#tyk07n)"
+                        stroke="#fff"
+                        filter="url(#s2i7rr)"
+                      />
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#tyk07n)"
+                        stroke="#999"
+                        strokeWidth={0}
+                        strokeLinejoin="miter"
+                      />
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#tyk07n)"
+                      />
+                    </svg>
+                  </span>
+                  {/* Star 4 */}
+                  <span
+                    data-v-34cbeed1=""
+                    className="vue-star-rating-star"
+                    style={{ marginRight: 0 }}
+                  >
+                    <svg
+                      data-v-21f5376e=""
+                      data-v-34cbeed1=""
+                      height={14}
+                      width={14}
+                      viewBox="0 0 14 14"
+                      className="vue-star-rating-star"
+                      step={10}
+                    >
+                      <linearGradient
+                        data-v-21f5376e=""
+                        id="pxzd3"
+                        x1={0}
+                        x2="100%"
+                        y1={0}
+                        y2={0}
+                      >
+                        <stop
+                          data-v-21f5376e=""
+                          offset="100%"
+                          stopColor="#faad1d"
+                        />
+                        <stop
+                          data-v-21f5376e=""
+                          offset="100%"
+                          stopColor="#d8d8d8"
+                        />
+                      </linearGradient>
+                      <filter
+                        data-v-21f5376e=""
+                        id="dk9hs"
+                        height="130%"
+                        width="130%"
+                        filterUnits="userSpaceOnUse"
+                      >
+                        <feGaussianBlur
+                          data-v-21f5376e=""
+                          stdDeviation={0}
+                          result="coloredBlur"
+                        />
+                        <feMerge data-v-21f5376e="">
+                          <feMergeNode data-v-21f5376e="" in="coloredBlur" />
+                          <feMergeNode data-v-21f5376e="" in="SourceGraphic" />
+                        </feMerge>
+                      </filter>
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#pxzd3)"
+                        stroke="#fff"
+                        filter="url(#dk9hs)"
+                      />
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#pxzd3)"
+                        stroke="#999"
+                        strokeWidth={0}
+                        strokeLinejoin="miter"
+                      />
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#pxzd3)"
+                      />
+                    </svg>
+                  </span>
+                  {/* Star 5 */}
+                  <span
+                    data-v-34cbeed1=""
+                    className="vue-star-rating-star"
+                    style={{ marginRight: 0 }}
+                  >
+                    <svg
+                      data-v-21f5376e=""
+                      data-v-34cbeed1=""
+                      height={14}
+                      width={14}
+                      viewBox="0 0 14 14"
+                      className="vue-star-rating-star"
+                      step={10}
+                    >
+                      <linearGradient
+                        data-v-21f5376e=""
+                        id="z8j06b"
+                        x1={0}
+                        x2="100%"
+                        y1={0}
+                        y2={0}
+                      >
+                        <stop
+                          data-v-21f5376e=""
+                          offset="30%"
+                          stopColor="#faad1d"
+                        />
+                        <stop
+                          data-v-21f5376e=""
+                          offset="30%"
+                          stopColor="#d8d8d8"
+                        />
+                      </linearGradient>
+                      <filter
+                        data-v-21f5376e=""
+                        id="sntkak"
+                        height="130%"
+                        width="130%"
+                        filterUnits="userSpaceOnUse"
+                      >
+                        <feGaussianBlur
+                          data-v-21f5376e=""
+                          stdDeviation={0}
+                          result="coloredBlur"
+                        />
+                        <feMerge data-v-21f5376e="">
+                          <feMergeNode data-v-21f5376e="" in="coloredBlur" />
+                          <feMergeNode data-v-21f5376e="" in="SourceGraphic" />
+                        </feMerge>
+                      </filter>
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#z8j06b)"
+                        stroke="#fff"
+                        filter="url(#sntkak)"
+                      />
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#z8j06b)"
+                        stroke="#999"
+                        strokeWidth={0}
+                        strokeLinejoin="miter"
+                      />
+                      <polygon
+                        data-v-21f5376e=""
+                        points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14"
+                        fill="url(#z8j06b)"
+                      />
+                    </svg>
+                  </span>
+                </div>
+              </div>
+              <span className="reviews-count">(1593)</span>
+            </div>
+          </div>
+          <div className="rest-logo image-ratio ratio-square radius">
+            <div className="ratio-content image-loading image-loaded">
+              <img
+                alt="restaurant.data.name"
+                data-alt="Chili's"
+                className="b-lazy"
+                data-src="https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Thumbnail/15ecd61e-51fe-44be-9e09-46d6d6cd1f9e.jpg"
+                src="https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Thumbnail/15ecd61e-51fe-44be-9e09-46d6d6cd1f9e.jpg"
+                lazy="loaded"
+              />
+            </div>
+          </div>
+          <div className="main-data">
+            <h3 className="card-title">
+              <a href="/cairo/chilis-79yv" className="text-decoration-none">
+                Chili's
+              </a>
+            </h3>
+            <ul className="cuisines-list h-dots-list truncate p-0 ">
+              <li className="list-item ">American</li>
+            </ul>
+            <span title="staticContent.filters.onlinePayment">
+              <span className="icon online-payment-icon">
+                <IoMdCard />
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className="card-footer clickable-item">
+          <a href="/cairo/popcity--lqxro" className="clickable-anchor" />
+          <span>
+            <span className="icon">
+              <TbTruckDelivery />
+            </span>
+            60 mins
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EltestBox;
