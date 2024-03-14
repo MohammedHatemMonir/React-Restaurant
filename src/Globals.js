5a7110bb514d02dff8e00329e1a13266a0733741
Mohammed Hatem Monir <atmelshiekh@yahoo.com>
2024-03-14 23:23:14 +0200

    fixed minor frontend bug

--- src/Globals.js
+++ src/Globals.js
@@ -1,4 +1,4 @@
 import { signal } from "@preact/signals-react"
 
 export const isDarkMode = signal(localStorage.getItem('darkMode') == 'true'? true: false);
-export const UserData = signal( {loggedIn:false});
+export const UserData = signal({loggedIn:true});
