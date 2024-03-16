59d0d21fc2b91ff1ae34714ba6e65c5f01d1696c
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-03-16 08:32:30 +0200

    Optimized

--- src/components/Map/LeafletMap.jsx
+++ src/components/Map/LeafletMap.jsx
@@ -9,7 +9,7 @@ function LeafletMap() {
       <MapContainer
         center={[51.505, -0.09]}
         zoom={13}
-        style={{ height: "400px", width: "100%" }}
+        style={{ height: "100vh", width: "100%" }}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
