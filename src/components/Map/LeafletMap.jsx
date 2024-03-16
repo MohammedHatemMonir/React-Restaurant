bb02e539b22cb47fe7fbeb986024d861613e7ca0
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-03-16 14:22:10 +0200

    Draggable marker added in map

--- src/components/Map/LeafletMap.jsx
+++ src/components/Map/LeafletMap.jsx
@@ -3,9 +3,13 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function LeafletMap() {
+  const handleMarkerDragEnd = (event) => {
+    console.log("New marker position:", event.target.getLatLng());
+  };
+
   return (
-    <div className="App">
-      <h1>Leaflet Map</h1>
+    <div>
+      <h1 className="text-center p-2">Leaflet Map</h1>
       <MapContainer
         center={[51.505, -0.09]}
         zoom={13}
@@ -15,7 +19,13 @@ function LeafletMap() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker
+          position={[51.505, -0.09]}
+          draggable={true}
+          eventHandlers={{
+            dragend: handleMarkerDragEnd,
+          }}
+        >
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
