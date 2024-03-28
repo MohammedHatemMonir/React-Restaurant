5cbf32a0afa1787ce56e503bfa5f0e6d174ae4a3
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-03-28 14:47:06 +0200

    Orders page created with json file for data ex

--- src/components/Orders/OrdersPage.jsx
+++ src/components/Orders/OrdersPage.jsx
@@ -1,4 +1,7 @@
-const OrdersTable = ({ orders }) => {
+import OrdersData from "./OrdersData.json";
+import EmptyOrders from "./EmptyOrders";
+
+const OrdersTable = () => {
   return (
     <div className="container">
       <h2 className="text-center font-weight-bold my-4">Orders</h2>
@@ -16,12 +19,19 @@ const OrdersTable = ({ orders }) => {
             </tr>
           </thead>
           <tbody>
-            {orders.map(order => (
+            {OrdersData.map((order) => (
               <tr key={order.id}>
                 <td>{order.id}</td>
                 <td>{order.customerName}</td>
                 <td>{order.productName}</td>
-                <td><img src={order.mealImage} alt="Meal" className="img-thumbnail" style={{ maxWidth: '100px', maxHeight: '100px' }} /></td>
+                <td>
+                  <img
+                    src={order.mealImage}
+                    alt="Meal"
+                    className="img-thumbnail"
+                    style={{ maxWidth: "100px", maxHeight: "100px" }}
+                  />
+                </td>
                 <td>{order.quantity}</td>
                 <td>${order.totalPrice.toFixed(2)}</td>
                 <td>{order.orderStatus}</td>
@@ -32,45 +42,10 @@ const OrdersTable = ({ orders }) => {
       </div>
     </div>
   );
-}
-
-// Example orders data
-const orders = [
-  {
-    id: 1,
-    customerName: 'John Doe',
-    productName: 'Product A',
-    mealImage: 'url_to_your_image1.jpg',
-    quantity: 2,
-    totalPrice: 25.99,
-    orderStatus: 'In Progress'
-  },
-  {
-    id: 2,
-    customerName: 'Jane Smith',
-    productName: 'Product B',
-    mealImage: 'url_to_your_image2.jpg',
-    quantity: 1,
-    totalPrice: 15.49,
-    orderStatus: 'Delivered'
-  },
-  {
-    id: 3,
-    customerName: 'Michael Johnson',
-    productName: 'Product C',
-    mealImage: 'url_to_your_image3.jpg',
-    quantity: 3,
-    totalPrice: 35.75,
-    orderStatus: 'Pending'
-  }
-];
+};
 
 const OrdersPage = () => {
-  return (
-    <div>
-      <OrdersTable orders={orders} />
-    </div>
-  );
-}
+  return <div>{OrdersData.length > 0 ? <OrdersTable /> : <EmptyOrders />}</div>;
+};
 
 export default OrdersPage;
