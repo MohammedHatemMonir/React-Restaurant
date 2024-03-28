50e254231189786342c0956b4849c659cdb80cbb
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-03-28 21:10:38 +0200

    Unnecessary images deleted

--- src/ReviewsData.js
+++ src/ReviewsData.js
@@ -1,13 +1,7 @@
-import person1 from "./utils/img/person1.jpg";
-import person2 from "./utils/img/person2.jpg";
-import person3 from "./utils/img/person3.jpg";
-import person4 from "./utils/img/person4.jpg";
-
 const reviewsData = [
   {
     id: 1,
     name: "John Mike",
-    img: person1,
     comment:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit.  iste culpa perspiciatis. Magnam, explicabo cumque.",
     emotion: 4,
@@ -15,7 +9,6 @@ const reviewsData = [
   {
     id: 2,
     name: "Michael Evans",
-    img: person2,
     comment:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores, mollitia?",
     emotion: 1,
@@ -23,7 +16,6 @@ const reviewsData = [
   {
     id: 3,
     name: "Robert Johnson",
-    img: person3,
     comment:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolor itaque reprehenderit minus tempore. Iste quibusdam facilis excepturi nihil maiores!",
     emotion: 3,
@@ -31,7 +23,6 @@ const reviewsData = [
   {
     id: 4,
     name: "Nick Burn",
-    img: person4,
     comment:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum et voluptate minus error suscipit officiis placeat repudiandae quibusdam officia tempora, reprehenderit, enim, quidem exercitationem laborum!",
     emotion: 5,
