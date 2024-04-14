14812676e134e5d7804f07cc4e35719ecc931327
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-04-14 14:57:27 +0200

    Update HemaTest.jsx

--- src/components/TestComponents/HemaTest.jsx
+++ src/components/TestComponents/HemaTest.jsx
@@ -1,116 +1,53 @@
-import { Col, Form } from "react-bootstrap";
-import { useMutation } from "react-query";
-import { apiClient } from "../../Data/apiclient";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Navbar, Nav, Container, Image } from "react-bootstrap";
+import { FaShoppingCart, FaMapMarkerAlt, FaSearch } from "react-icons/fa";
 
-const SearchComponent = () => {
-  const searchTerm = useSignal("");
-  const searchResults = useSignal([]);
-
-  const handleSearchChange = (e) => {
-    searchTerm.value = e.target.value;
-    console.log(searchTerm.value);
-    returnUsers();
-  };
-
-  const searchUsers = useMutation({
-    mutationKey: ["searchUsers"],
-    mutationFn: async (params) => {
-      let url = "/SearchUser";
-      return await apiClient.post(url, params);
-    },
-  });
-
-  const returnUsers = async () => {
-    if (searchTerm.value.length >= 3) {
-      const result = await searchUsers.mutateAsync({
-        name: searchTerm.value,
-      });
-
-      console.log("Search result", result.data);
-      searchResults.value = result.data;
-    } else {
-      searchResults.value = [];
-    }
-  };
-
+const NavigationBar = () => {
   return (
-    <Col sm={12}>
-      <Form.Group className="mb-2 mb-sm-0">
-        <Form.Label className="dropdown">Search user</Form.Label>
-        <Form.Control
-          type="text"
-          name="search-user"
-          placeholder="Search User"
-          value={searchTerm.value}
-          onChange={handleSearchChange}
-        />
-
-        <div className="dropdown-content">
-          {searchResults.value.map((user, index) => (
-            <Link key={index} className="d-block text-decoration-none">
-              {user.name}
-            </Link>
-          ))}
-        </div>
-      </Form.Group>
-    </Col>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src="/logo.png"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">
+              <FaMapMarkerAlt /> Location
+            </Nav.Link>
+            <Nav.Link href="#link">
+              <FaShoppingCart /> Cart
+            </Nav.Link>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link href="#link">
+              <FaSearch /> Search
+            </Nav.Link>
+            <Nav.Link href="#link">
+              <Image
+                src="https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png"
+                roundedCircle
+                style={{ width: "30px", height: "30px" }}
+                alt="Profile"
+              />
+            </Nav.Link>
+            <Nav.Link href="#link">Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default SearchComponent;
-
-// import React from "react";
-// import { Navbar, Nav, Container, Image } from "react-bootstrap";
-// import { FaShoppingCart, FaMapMarkerAlt, FaSearch } from "react-icons/fa";
-
-// const NavigationBar = () => {
-//   return (
-//     <Navbar bg="light" expand="lg">
-//       <Container>
-//         <Navbar.Brand href="#home">
-//           <img
-//             src="/logo.png"
-//             height="30"
-//             className="d-inline-block align-top"
-//             alt="Logo"
-//           />
-//         </Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="mr-auto">
-//             <Nav.Link href="#home">
-//               <FaMapMarkerAlt /> Location
-//             </Nav.Link>
-//             <Nav.Link href="#link">
-//               <FaShoppingCart /> Cart
-//             </Nav.Link>
-//           </Nav>
-//           <Nav className="ml-auto">
-//             <Nav.Link href="#link">
-//               <FaSearch /> Search
-//             </Nav.Link>
-//             <Nav.Link href="#link">
-//               <Image
-//                 src="https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png"
-//                 roundedCircle
-//                 style={{ width: "30px", height: "30px" }}
-//                 alt="Profile"
-//               />
-//             </Nav.Link>
-//             <Nav.Link href="#link">Logout</Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// };
-
-// export default NavigationBar;
+export default NavigationBar;
 
 // import React from "react";
 // import { useForm } from "react-hook-form";
-import { useSignal } from "@preact/signals-react";
 
 // function MyForm() {
 //   const {
