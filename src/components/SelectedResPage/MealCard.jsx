d2900c883d92596e9ee26e0ae1ddbc95fa631fba
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-04-20 01:55:24 +0200

    Update MealCard.jsx

--- src/components/SelectedResPage/MealCard.jsx
+++ src/components/SelectedResPage/MealCard.jsx
@@ -1,25 +1,12 @@
 import Stars from "../Stars/Stars";
-import { UserData } from "../../Globals";
+import { Cart, UserData } from "../../Globals";
 import { useSignal } from "@preact/signals-react";
 import MealDetailsModal from "./MealDetailsModal";
 import DeleteIcon from "./../../Icons/DeleteIcon";
 import EditIcon from "./../../Icons/EditIcon";
 import DeleteMealModal from "./DeleteMealModal";
 import EditMealModal from "./EditMealModal";
-import "../ResPage/ResCard.scss";
-import { Link } from "react-router-dom";
-import {
-  FaCartPlus,
-  FaCompass,
-  FaPlus,
-  FaPlusSquare,
-  FaReadme,
-  FaTag,
-} from "react-icons/fa";
-import { IoSettings } from "react-icons/io5";
-import { CiSquarePlus } from "react-icons/ci";
-import { HiPlusCircle } from "react-icons/hi2";
-import { IoMdArrowDropdownCircle } from "react-icons/io";
+
 export default function MealCard({
   id,
   name,
@@ -36,7 +23,7 @@ export default function MealCard({
     displayModal.value = false;
   }
 
-  // Hema Popup Modals Here
+  // Hema Here
   const showDelModal = useSignal(false);
   const showEditModal = useSignal(false);
 
@@ -44,108 +31,165 @@ export default function MealCard({
   function onOpenDel() {
     showDelModal.value = true;
   }
-
-  function onCloseDel() {
-    showDelModal.value = false;
-  }
-
   function onOpenEdit() {
     showEditModal.value = true;
   }
 
+  function onCloseDel() {
+    showDelModal.value = false;
+  }
   function onCloseEdit() {
     showEditModal.value = false;
   }
 
   return (
-    <section
-      className="text-decoration-none blogs"
-      id="blogs"
-      style={{ transform: "scale(0.85)" }}
+    <div
+      style={{
+        backgroundColor: "#ffffff",
+        borderRadius: "0.5rem",
+        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+        overflow: "hidden",
+        margin: "0.75rem 0",
+        cursor: "pointer",
+      }}
     >
-      <div className="text-decoration-none box-container">
-        <div className="text-decoration-none box">
-          <div className="image">
-            <br />
-            <h3 style={{ background: "white" }}>
-              <Stars stars1={rating} /> <br />
-              <br />
-            </h3>
-            <img
-              src={
-                mealImg ||
-                `https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Normal/92edf264-8a29-4efb-997a-ca2fdf9b3a92.jpg`
-              }
-              alt="Res-Img"
-            />
-          </div>
-          <div className="text-decoration-none content">
-            <div className="text-decoration-none tags">
-              <Link>
-                <FaTag className="text-decoration-none" /> food /
-              </Link>
-              <Link>
-                <FaTag className="text-decoration-none" /> burger /
-              </Link>
-              <Link>
-                <FaTag className="text-decoration-none" /> pizza
-              </Link>
-            </div>
-            <h3 style={{ fontSize: "25px" }}>{name}</h3>
-            <p>{desc}</p>
-            <Link
-              to={`/restaurant/${id}/${name}`}
-              className="w-100 btn"
-              style={{ marginTop: "-10px" }}
+      {/* Meal Img */}
+      <img
+        onClick={() => (displayModal.value = true)}
+        src={mealImg}
+        alt={mealImg}
+        style={{
+          width: "100%",
+          height: "150px",
+          objectFit: "cover",
+          border: "none",
+        }}
+        className="meal-card__image"
+      />
+      <div style={{ padding: "1rem" }} className="meal-card__content">
+        {/* Modal Here */}
+        <MealDetailsModal
+          id={id}
+          openModal={displayModal.value}
+          closeModal={handleClose}
+          mealName={name}
+          mealDesc={desc}
+          mealImg={mealImg}
+          mealPrice={price}
+          MealComments={MealComments}
+          resID={resID}
+          resName={resName}
+          rating={rating}
+          desc={desc}
+          price={price}
+          name={name}
+        />
+        <h3
+          style={{
+            fontSize: "1.125rem",
+            fontWeight: "600",
+            marginBottom: "0.5rem",
+          }}
+          className="meal-card__title"
+        >
+          {name}
+        </h3>
+        <p
+          style={{
+            color: "#4b5563",
+            marginBottom: "1rem",
+          }}
+          className="meal-card__description"
+        >
+          {desc}
+        </p>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+          className="meal-card__footer"
+        >
+          <div>
+            <p
+              style={{
+                color: "#dc2626",
+                fontSize: "1.125rem",
+                fontWeight: "600",
+                margin: "0",
+              }}
+              className="meal-card__price"
             >
-              <span>
-                <IoMdArrowDropdownCircle style={{ color: "white" }} />
-              </span>
-              <span> Read More</span>
-            </Link>
-            {UserData.value.role == "ADMIN" && (
-              <div className="position-relative">
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-around",
-                    alignItems: "center",
-                    cursor: "pointer",
-                    // marginTop: "30px",
-                    // marginRight: "-20px",
-                    // objectFit: "content",
-                    margin: "25px -10% 0px  -10%",
-                    transform: "scale(1.5)",
-                  }}
-                >
-                  <div onClick={onOpenDel}>
-                    <DeleteIcon />
-                  </div>
-                  <div onClick={onOpenEdit}>
-                    <IoSettings />
-                  </div>
-                </div>
+              <div className="font-weight-bold">
+                <span className="text-danger"> {price}</span>
+                <span className="text-dark"> $</span>
               </div>
-            )}
+            </p>
           </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginTop: "-60px",
+            }}
+            className="meal-card__rating"
+          >
+            <Stars stars1={rating} />
+          </div>
+          {/* <button
+            style={{
+              backgroundColor: "#e32207",
+              color: "#fff",
+              border: "none",
+              padding: "0.5rem 1rem",
+              borderRadius: "0.25rem",
+              cursor: "pointer",
+              transition: "background-color 0.3s",
+            }}
+            onClick={AddToCart}
+            className="btn meal-card__button"
+          >
+            Buy
+          </button> */}
         </div>
+        {UserData.value.role == "ADMIN" && (
+          <div className="position-relative">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                transform: "scale(1.5)",
+                margin: "15px 17% 10px 17%",
+              }}
+            >
+              <div onClick={onOpenDel} className="">
+                <DeleteIcon className="delete-icon" />
+              </div>
+              <div onClick={onOpenEdit} className="">
+                <EditIcon className="edit-icon" />
+              </div>
+            </div>
+          </div>
+        )}
       </div>
       {UserData.value.role == "ADMIN" && showDelModal.value == true && (
-        <DeleteResModal
-          resId={id}
-          resName={name}
+        <DeleteMealModal
+          mealId={id}
+          mealName={name}
           openModal={() => (showDelModal.value = true)}
           closeModal={onCloseDel}
         />
       )}
       {UserData.value.role == "ADMIN" && showEditModal.value == true && (
-        <EditResModal
-          resName={name}
-          resId={id}
+        <EditMealModal
+          mealId={id}
+          mealName={name}
           openModal={() => (showEditModal.value = true)}
           closeModal={onCloseEdit}
         />
       )}
-    </section>
+    </div>
   );
 }
