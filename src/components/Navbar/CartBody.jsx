259cec15f6e67baf4b60691b306f604e5301459a
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-04-21 19:25:31 +0200

    Revert "Hot fixes"
    This reverts commit 59a6f002651fae75be936cacea83d067d074dea2.

--- src/components/Navbar/CartBody.jsx
+++ src/components/Navbar/CartBody.jsx
@@ -46,18 +46,23 @@ export default function CartBody() {
       <div className="products-container">
         <h3 className="title">My Products</h3>
         <div className="box-container">
-          {m.isSuccess &&
+          {!m.isLoading &&
             !m.isLoading &&
             Cart.value.meals?.map((meal, index) => (
-              <div className="box" key={index + meal.id}>
-                <FaTimes className="fas fa-times" key={index} />
+              <div className="box" key={index}>
+                <FaTimes className="fas fa-times" />
+                {console.log("meal data", meal)}
+                {/* // meal.mealImg || */}
                 <img src={meal.mealImg} alt="meal-img" />
                 <div className="content">
+                  {/* meal.name || */}
                   <h3>{meal.name}</h3>
                   <span> quantity : </span>
+                  {/* meal.quantity || */}
                   <input type="number" defaultValue={meal.quantity} />
                   <br />
                   <span> price : </span>
+                  {/* meal.price || */}
                   <span className="price"> {meal.price} </span>
                 </div>
               </div>
