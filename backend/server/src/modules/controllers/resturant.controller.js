7ba57ed720dc9ccfa3af91c3b4d0075fc48e31bc
mostafamahmoud21 <152217975+mostafamahmoud21@users.noreply.github.com>
2024-05-25 15:42:58 +0300

    k

--- backend/server/src/modules/controllers/resturant.controller.js
+++ backend/server/src/modules/controllers/resturant.controller.js
@@ -190,7 +190,7 @@ const updateRestaurant = async (req, res) => {
         const userId = req.session?.user?._id;
         const checkOwner = await restaurant.findOne({ _id: id, ownerId: userId });
         console.log(checkOwner)
-        if (req.session.user.role != "ADMIN"||!checkOwner) {
+        if (!checkOwner) {
             return res.status(403).json({ error: "Unauthorized" });
         }
         
