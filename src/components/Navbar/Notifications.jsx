f30644c79304cb11901946926012da3cba5d3c67
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-05-31 21:59:41 +0300

    Local storage removed

--- src/components/Navbar/Notifications.jsx
+++ src/components/Navbar/Notifications.jsx
@@ -4,42 +4,26 @@ import { Dropdown, Badge } from "react-bootstrap";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { io } from "socket.io-client";
 import { useNavigate } from "react-router-dom";
-import { UserData } from "../../Globals";
 
 const NotificationDropdown = () => {
   const navigate = useNavigate();
   const displayNewNotifications = useSignal("none");
-
-  // Get notification from local storage
-
-  const notifications = useSignal(
-    localStorage.getItem(`myData-${UserData.value.id}`)
-      ? JSON.parse(localStorage.getItem(`myData-${UserData.value.id}`))
-      : []
-  );
-  // console.log("medooo", localStorage.getItem(`myData-${UserData.value.id}`));
-  // console.log("UserData.value.id", UserData.value.id);
+  const notifications = useSignal([]);
   const newNotifications = useSignal(0);
   const socket = io("http://localhost:5001", { withCredentials: true });
-  console.log("notifif", notifications.value);
+
   function onNotification(data) {
     console.log("Received new notification:", data);
     notifications.value = [data, ...notifications.value];
-
     newNotifications.value = newNotifications.value + 1;
     newNotifications.value > 0
       ? (displayNewNotifications.value = "")
       : (displayNewNotifications.value = "none");
 
-    // Set notification in local storage
-    localStorage.setItem(
-      `myData-${UserData.value.id}`,
-      JSON.stringify(notifications.value.slice(-5))
-    );
-    console.log(JSON.stringify(notifications.value.slice(-5)));
+    // console.log("notifications.value", notifications.value);
   }
-
   console.log("newNotifications.value", newNotifications.value);
+  // console.log("notifications.value", notifications.value);
 
   useEffect(() => {
     function onConnect() {
@@ -74,8 +58,7 @@ const NotificationDropdown = () => {
 
       <Dropdown.Menu style={{ minWidth: "300px" }}>
         <Dropdown.Header>Notifications</Dropdown.Header>
-        {/* Edited here */}
-        {notifications.value?.length === 0 ? (
+        {notifications.value.length === 0 ? (
           <Dropdown.Item text>No new notifications</Dropdown.Item>
         ) : (
           notifications.value?.map((notification, index) => (
